b7bd39e86990e0becf3d8dea511ec34eae99dfe3
Filippos Makridis <makridistaker@hotmail.gr>
2021-08-24 01:30:06 +0300

    Basic version of slider bind to label

--- Exercises/HelloWorldFX/src/sample/Controller.java
+++ Exercises/HelloWorldFX/src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableStringValue;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+
 public class Controller {
+
+    public Controller() {
+    }
+    @FXML
+    private Slider slider;
+    @FXML
+    private Label label;
+
+    @FXML
+    private void actionTest(){
+        Main m = new Main();
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                label.textProperty().setValue(String.valueOf(newValue));
+            }
+        });
+
+    }
+
 }
--- Exercises/HelloWorldFX/src/sample/Main.java
+++ Exercises/HelloWorldFX/src/sample/Main.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +16,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
+
     }
+
 
 
     public static void main(String[] args) {
